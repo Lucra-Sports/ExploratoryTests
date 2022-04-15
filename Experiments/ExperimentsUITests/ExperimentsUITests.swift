30fbb2a2bc69243b4763b2d5297460cf0b6290cb
Paul Zabelin <paulz@users.noreply.github.com>
2022-04-14 22:06:03 -0700

    add testDateFormatterRace

--- Experiments/ExperimentsUITests/ExperimentsUITests.swift
+++ Experiments/ExperimentsUITests/ExperimentsUITests.swift
@@ -8,11 +8,37 @@
 import XCTest
 
 class ExperimentsUITests: XCTestCase {
+    let app = XCUIApplication()
+    let timeout: TimeInterval = 10
+
     func testDateFormatterRace() throws {
-        let app = XCUIApplication()
         app.launch()
-        XCTAssertTrue(app.wait(for: .runningForeground, timeout: 30))
-        app.buttons["Race DateFormatter"].tap()
-        XCTAssertTrue(app.wait(for: .notRunning, timeout: 30))
+        XCTAssertTrue(app.wait(for: .runningForeground, timeout: timeout))
+        try verifyAppCrash {
+            app.buttons["Race DateFormatter"].tap()
+        }
+    }
+    
+    func expectToCrash() throws -> XCTestExpectation {
+        let processId = pid_t(try XCTUnwrap(app.value(forKey: "processID") as? Int32))
+        
+        let source = DispatchSource.makeProcessSource(
+            identifier: processId,
+            eventMask: .signal
+        )
+        source.activate()
+        
+        let appCrashed = expectation(description: "app crashed")
+        source.setEventHandler {
+            appCrashed.fulfill()
+        }
+        return appCrashed
+    }
+    
+    func verifyAppCrash(_ block: ()->Void) throws {
+        let appCrashed = try expectToCrash()
+        block()
+        wait(for: [appCrashed], timeout: timeout)
+        XCTAssertTrue(app.wait(for: .notRunning, timeout: timeout))
     }
 }
