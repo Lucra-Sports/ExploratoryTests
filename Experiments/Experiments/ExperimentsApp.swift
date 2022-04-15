0660b6394819514a1464f251b0b07ee15b58f4a0
Paul Zabelin <paulz@users.noreply.github.com>
2022-04-14 21:40:42 -0700

    creat SwiftUI iOS app project

---
+++ Experiments/Experiments/ExperimentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExperimentsApp.swift
+//  Experiments
+//
+//  Created by Paul Zabelin on 4/14/22.
+//
+
+import SwiftUI
+
+@main
+struct ExperimentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Experiments/ExperimentsUITests/ExperimentsUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ExperimentsUITestsLaunchTests.swift
+//  ExperimentsUITests
+//
+//  Created by Paul Zabelin on 4/14/22.
+//
+
+import XCTest
+
+class ExperimentsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
