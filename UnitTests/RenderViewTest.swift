71257f7ff92dbd56efa0b511f6f7c91952d5c3eb
Paul Zabelin <paulz@users.noreply.github.com>
2022-05-22 03:30:46 -0700

    render SwiftUI view in a window

--- UnitTests/RenderViewTest.swift
+++ UnitTests/RenderViewTest.swift
@@ -28,16 +28,24 @@ class RenderViewTest: XCTestCase {
         try png.write(to: URL(fileURLWithPath: "/tmp/sampleView.png"))
     }
     
-    func testSwiftUIRendersBlank() throws {
+    func testSwiftUIRendersInWindow() throws {
+        let window = UIWindow()
+        window.makeKeyAndVisible()
+        let rootController = UIViewController()
+        window.rootViewController = rootController
         let controller = UIHostingController(rootView: SampleView())
         let view = controller.view!
-        view.frame = .init(origin: .zero, size: view.intrinsicContentSize)
+        let size = view.intrinsicContentSize
+        let safeOrigin = rootController.view.safeAreaLayoutGuide.layoutFrame.origin
+        view.frame = .init(origin: safeOrigin, size: size)
+        rootController.addChild(controller)
+        rootController.view.addSubview(controller.view)
         let image = try XCTUnwrap(view.renderLayerAsBitmap())
         XCTAssertNotNil(image)
         XCTAssertEqual(image.size, .init(width: 30, height: 20))
         let png = try XCTUnwrap(image.pngData())
         let existing = try Data(
-            contentsOf: folderUrl().appendingPathComponent("sampleSwiftUIView-blank.png")
+            contentsOf: folderUrl().appendingPathComponent("sampleSwiftUIView.png")
         )
         XCTAssertEqual(existing, png)
         try png.write(to: URL(fileURLWithPath: "/tmp/sampleSwiftUIView.png"))
